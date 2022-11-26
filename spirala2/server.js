c59f36e43dca97930fb73fe0d8578283d0587566
Edo Cicak <edocicak@gmail.com>
2022-11-26 15:05:22 +0100

    popravljen res code

--- spirala2/server.js
+++ spirala2/server.js
@@ -296,7 +296,7 @@ const server = http
           res.end(JSON.stringify({ message: "Prisustvo ne postoji!" }));
           return;
         } else {
-          res.writeHead(400, { "Content-Type": "application/json" });
+          res.writeHead(200, { "Content-Type": "application/json" });
           res.end(
             JSON.stringify({
               prisustvoZaSedmicu: objekat.sedmica,
