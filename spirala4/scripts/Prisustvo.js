d95a8d53266b80b42fe07c40d69d6208561cf3a6
Burdock <edocicak@gmail.com>
2023-01-20 15:25:05 +0100

    Ispravljene greske iz spirale 1, Prisustvo.js

--- spirala4/scripts/Prisustvo.js
+++ spirala4/scripts/Prisustvo.js
@@ -1,120 +1,120 @@
 class Prisustvo {
-  static trenutnaSedmica;
+    static trenutnaSedmica;
 
-  constructor() {
-    Prisustvo.trenutnaSedmica = 1;
-    this.prisustvo = 0;
-    this.finalnoStanje = false;
-  }
-
-  izracunajPrisustvo(sedmica, listaPrisustva) {
-    if (sedmica < 1 || sedmica > 15)
-      return {
-        greska: "Parametar sedmica nema vrijednost u rasponu od 1 do 15!",
-      };
-    if (sedmica > Prisustvo.trenutnaSedmica)
-      return {
-        greska:
-          "Parametar sedmica mora imati vrijednost koja je manja od trenutne sedmice!",
-      };
-
-    for (let x of listaPrisustva) {
-      if (
-        Object.keys(x).length != 4 ||
-        Object.getOwnPropertyNames(x)[0] != "prSedmica" ||
-        Object.getOwnPropertyNames(x)[1] != "prisutan" ||
-        Object.getOwnPropertyNames(x)[2] != "odsutan" ||
-        Object.getOwnPropertyNames(x)[3] != "nijeUneseno" ||
-        !Number.isInteger(x.prSedmica) ||
-        !Number.isInteger(x.prisutan) ||
-        !Number.isInteger(x.odsutan) ||
-        !Number.isInteger(x.nijeUneseno)
-      )
-        return { greska: "Parametar listaPrisustva nema ispravne properties!" };
-    }
-    const unikatni = [];
-    const unazad = listaPrisustva.slice(0);
-    unazad.reverse();
-
-    const novaLista = unazad.filter((element) => {
-      const isDuplicate = unikatni.includes(element.prSedmica);
-
-      if (!isDuplicate) {
-        unikatni.push(element.prSedmica);
-        return true;
-      }
-
-      return false;
-    });
-    novaLista.reverse();
-
-    let greskeAtributi = "";
-    let imaGreska = false;
-    let sedmicaSaGreskom;
-
-    for (let x of novaLista) {
-      if (x.prisutan < 0 || x.prisutan > 8) {
-        greskeAtributi += "prisutan,";
-        imaGreska = true;
-      }
-      if (x.odsutan < 0 || x.odsutan > 8) {
-        greskeAtributi += "odsutan,";
-        imaGreska = true;
-      }
-      if (x.nijeUneseno < 0 || x.nijeUneseno > 8) {
-        greskeAtributi += "nijeUneseno,";
-        imaGreska = true;
-      }
-      if (imaGreska) {
-        sedmicaSaGreskom = x.prSedmica;
-        break;
-      }
-    }
-    greskeAtributi = greskeAtributi.slice(0, -1);
-
-    if (imaGreska) {
-      return {
-        greska:
-          "Parametar listaPrisustva nema ispravne vrijednosti za sedmicu " +
-          sedmicaSaGreskom +
-          " za properties [" +
-          greskeAtributi +
-          "]!",
-      };
+    constructor() {
+        Prisustvo.trenutnaSedmica = 1;
+        this.prisustvo = 0;
+        this.finalnoStanje = false;
     }
 
-    let ukupno_prisutan = 0;
-    let ukupno_odsutan = 0;
+    izracunajPrisustvo(sedmica, listaPrisustva) {
+        if (sedmica < 1 || sedmica > 15)
+            return {
+                greska: "Parametar sedmica nema vrijednost u rasponu od 1 do 15!",
+            };
+        if (sedmica > Prisustvo.trenutnaSedmica)
+            return {
+                greska:
+                    "Parametar sedmica mora imati vrijednost koja je manja od trenutne sedmice!",
+            };
 
-    this.finalnoStanje = true;
-    for (let x of novaLista) {
-      ukupno_prisutan += x.prisutan;
-      ukupno_odsutan += x.odsutan;
-      if (x.odsutan + x.prisutan + x.nijeUneseno > 8)
-        return {
-          greska: "Parametar listaPrisustva nema ispravnu zbirnu vrijednost!",
+        for (let x of listaPrisustva) {
+            if (
+                Object.keys(x).length != 4 ||
+                Object.getOwnPropertyNames(x)[0] != "prSedmica" ||
+                Object.getOwnPropertyNames(x)[1] != "prisutan" ||
+                Object.getOwnPropertyNames(x)[2] != "odsutan" ||
+                Object.getOwnPropertyNames(x)[3] != "nijeUneseno" ||
+                !Number.isInteger(x.prSedmica) ||
+                !Number.isInteger(x.prisutan) ||
+                !Number.isInteger(x.odsutan) ||
+                !Number.isInteger(x.nijeUneseno)
+            )
+                return {greska: "Parametar listaPrisustva nema ispravne properties!"};
+        }
+        const unikatni = [];
+        const unazad = listaPrisustva.slice(0);
+        unazad.reverse();
+
+        const novaLista = unazad.filter((element) => {
+            const isDuplicate = unikatni.includes(element.prSedmica);
+
+            if (!isDuplicate) {
+                unikatni.push(element.prSedmica);
+                return true;
+            }
+
+            return false;
+        });
+        novaLista.reverse();
+
+
+        let greskeAtributi = "";
+        let imaGreska = false;
+        let sedmicaSaGreskom;
+
+        for (let x of novaLista) {
+            if (x.prisutan < 0 || x.prisutan > 8) {
+                greskeAtributi += "prisutan,";
+                imaGreska = true;
+            }
+            if (x.odsutan < 0 || x.odsutan > 8) {
+                greskeAtributi += "odsutan,";
+                imaGreska = true;
+            }
+            if (x.nijeUneseno < 0 || x.nijeUneseno > 8) {
+                greskeAtributi += "nijeUneseno,";
+                imaGreska = true;
+            }
+            if (imaGreska) {
+                sedmicaSaGreskom = x.prSedmica;
+                break;
+            }
+        }
+        greskeAtributi = greskeAtributi.slice(0, -1);
+
+        if (imaGreska) {
+            return {
+                greska:
+                    "Parametar listaPrisustva nema ispravne vrijednosti za sedmicu " +
+                    sedmicaSaGreskom +
+                    " za properties [" +
+                    greskeAtributi +
+                    "]!",
+            };
+        }
+
+        let ukupno_prisutan = 0;
+        let ukupno_odsutan = 0;
+
+        this.finalnoStanje = true;
+        for (let x of novaLista) {
+            ukupno_prisutan += x.prisutan;
+            ukupno_odsutan += x.odsutan;
+            if (x.odsutan + x.prisutan + x.nijeUneseno > 8)
+                return {
+                    greska: "Parametar listaPrisustva nema ispravnu zbirnu vrijednost!",
+                };
+            if (x.nijeUneseno != 0) this.finalnoStanje = false;
+        }
+        this.prisustvo = ukupno_prisutan / (ukupno_prisutan + ukupno_odsutan);
+
+        let objekat = {
+            prisustvoZaSedmicu: sedmica,
+            prisutan: -1,
+            odsutan: -1,
+            nijeUneseno: -1,
         };
-      if (x.nijeUneseno != 0) this.finalnoStanje = false;
+        for (let x of novaLista) {
+            if (sedmica == x.prSedmica) {
+                objekat = {
+                    prisustvoZaSedmicu: sedmica,
+                    prisutan: x.prisutan,
+                    odsutan: x.odsutan,
+                    nijeUneseno: x.nijeUneseno,
+                };
+            }
+        }
+        return objekat;
     }
-    this.prisustvo = ukupno_prisutan / (ukupno_prisutan + ukupno_odsutan);
-
-    for (let x of novaLista) {
-      if (sedmica == x.prSedmica) {
-        return {
-          prisustvoZaSedmicu: sedmica,
-          prisutan: x.prisutan,
-          odsutan: x.odsutan,
-          nijeUneseno: x.nijeUneseno,
-        };
-      } else
-        return {
-          prisustvoZaSedmicu: sedmica,
-          prisutan: -1,
-          odsutan: -1,
-          nijeUneseno: -1,
-        };
-    }
-  }
 }
-
-module.exports = Prisustvo;
